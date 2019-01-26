0b46fbbe51f1f9bc3b458bba384cb69fdbe1c79a
Cooper Anderson <cooper@cooperstuff.ninja>
2019-01-25 19:25:27 -0500

    Add insert method

--- src/BinaryHeap.java
+++ src/BinaryHeap.java
@@ -16,8 +16,27 @@ public class BinaryHeap<T extends Comparable<? super T>> {
 		this.array = (T[]) Array.newInstance(this.classType, this.capacity + 1);
 	}
 
+	/**
+	 * Add a new item to the heap. Automatically gets put in the correct location.
+	 * @param item the new item being added to the heap
+	 */
 	public void insert(T item) {
-
+		if (this.size >= this.capacity) {
+			this.capacity *= 2;
+			T[] array = (T[]) Array.newInstance(this.classType, this.capacity + 1);
+			System.arraycopy(this.array, 1, array, 1, this.size);
+			this.resizeCount++;
+			this.array = array;
+		}
+		int index = this.size + 1;
+		this.array[index] = item;
+		this.size++;
+		while (index > 1 && this.array[index / 2].compareTo(item) > 0) {
+			this.array[index] = this.array[index / 2];
+			this.array[index / 2] = item;
+			this.swapCount++;
+			index /= 2;
+		}
 	}
 
 	public T deleteMin() {
